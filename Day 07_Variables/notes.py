457400fa8616e8d9742d67d8542e6f7dde66bf52
Nicholas Armstrong <narmstro@warren.k12.in.us>
2024-08-11 14:20:51 -0400

    Day 07

---
+++ Day 07_Variables/notes.py
@@ -0,0 +1,54 @@
+#Create a variable named greeting
+# and set its value to Hello World
+greeting1 = "Hello World"
+greeting2 = "Hi Again"
+print(greeting1)
+print(greeting2)
+greeting2 = "It's Me"
+print(greeting2)
+greeting2 = 3
+print(greeting2)
+
+an_integer = 3
+type_of_an_integer = type(an_integer)
+print("an_integer is of type", type_of_an_integer)
+
+a_float = -2.5
+type_of_a_float = type(a_float)
+print("a_float is of type", type_of_a_float)
+
+type_of_greeting1 = type(greeting1)
+print("type_of_greeting1 is of type", type_of_greeting1)
+
+large_integer = 12_356_789
+print("large_integer =", large_integer)
+
+hex_num = 0xF3
+print(hex_num)
+
+oct_num = 0o37
+print(oct_num)
+
+bin_num = 0b101010101110101
+print(bin_num)
+
+#3.84 * 10 ^ 4
+sci_notation_number = 3.84e4
+print(sci_notation_number)
+
+'''
+Python Data Types
+Covered today
+Text Type:	str
+Numeric Types:	int, float
+
+Covered at a later date
+Numeric Types:	complex
+Sequence Types:	list, tuple, range
+Mapping Type:	dict
+Set Types:	set, frozenset
+Boolean Type:	bool
+Binary Types:	bytes, bytearray, memoryview
+None Type:	NoneType
+
+'''
